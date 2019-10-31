cc3a8a0c2e29dc2efe6a7646b00949da3a635d4c
sly01 <aerkoc01@gmail.com>
2019-10-31 15:57:29 +0100

    jenkins managed by kubernetes

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ resource "kubernetes_namespace" "ns_development" {
   }
 }
 
-resource "kubernetes_deployment" "gogs-mysql" {
+resource "kubernetes_deployment" "jenkins" {
   metadata {
-    name = "gogs-mysql"
+    name = "jenkins"
     labels = {
-      app = "gogs-mysql"
+      app = "jenkins"
     }
   }
 
@@ -21,158 +21,74 @@ resource "kubernetes_deployment" "gogs-mysql" {
 
     selector {
       match_labels = {
-        app = "gogs-mysql"
+        app = "jenkins"
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "gogs-mysql"
+          app = "jenkins"
         }
       }
 
       spec {
         container {
-          image = "mysql:5.7"
-          name  = "gogs-mysql"
+          image = "jenkins/jenkins:lts"
+          name  = "jenkins"
 
           port {
-            container_port = 3306
+            container_port = 8080
           }
-          env {
-            name = "MYSQL_ROOT_PASSWORD"
-            value = "123456"
+          
+          port {
+            container_port = 50000
           }
-          env {
-            name = "MYSQL_DATABASE"
-            value = "gogs"
-          }
+
           volume_mount {
-            mount_path = "/var/lib/mysql"
-            name = "gogs-mysql-volume"
+            mount_path = "/var/jenkins_home"
+            name = "jenkins-volume"
           }
         }
         volume {
-            name = "gogs-mysql-volume"
+            name = "jenkins-volume"
             persistent_volume_claim {
-              claim_name = "${kubernetes_persistent_volume_claim.pvc_gogs-mysql.metadata.0.name}"
+              claim_name = "${kubernetes_persistent_volume_claim.pvc_jenkins.metadata.0.name}"
             }
           }
       }
     }
   }
 }
-
-resource "kubernetes_deployment" "gogs" {
+resource "kubernetes_service" "svc_jenkins" {
   metadata {
-    name = "gogs"
-    labels = {
-      app = "gogs"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "gogs"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "gogs"
-        }
-      }
-
-      spec {
-        # init_container {
-        #   name = "config-data"
-        #   image = "busybox"
-        #   command = ["sh", "-c", "chmod 600 -R /data/ssh"]
-        #   volume_mount {
-        #     mount_path = "/data"
-        #     name = "gogs-volume"
-        #   }
-        # }
-        container {
-          image = "gogs/gogs:latest"
-          name  = "gogs"
-
-          port {
-            container_port = 22
-          }
-          port {
-            container_port = 3000
-          }
-          env {
-            name = "RUN_CROND"
-            value = "true"
-          }
-          volume_mount {
-            mount_path = "/data"
-            name = "gogs-volume"
-          }
-        }
-        volume {
-            name = "gogs-volume"
-            host_path {
-              path = "/host_mnt/c/Users/Ahmet_Erkoc/.docker/Volumes/gogs-local"
-            }
-          }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "svc_gogs" {
-  metadata {
-    name = "gogs"
+    name = "jenkins"
   }
   spec {
     selector = {
-      app = "gogs"
+      app = "jenkins"
     }
     
     port {
       name        = "web-ui"
-      port        = 3000
-      target_port = 3000
-      node_port   = 32500
+      port        = 8080
+      target_port = 8080
+      node_port   = 32581
     }
 
     port {
-      name        = "ssh-access"
-      port        = 10022
-      target_port = 22
-      node_port   = 32501
+      name        = "build-executor"
+      port        = 50000
+      target_port = 50000
+      node_port   = 32582
     }
     type = "NodePort"
   }
 }
 
-resource "kubernetes_service" "svc_gogs-mysql" {
+resource "kubernetes_persistent_volume_claim" "pvc_jenkins" {
   metadata {
-    name = "gogs-mysql"
-  }
-  spec {
-    selector = {
-      app = "gogs-mysql"
-    }
-    
-    port {
-      name        = "web-ui"
-      port        = 3306
-    }
-    type = "ClusterIP"
-  }
-}
-resource "kubernetes_persistent_volume_claim" "pvc_gogs-mysql" {
-  metadata {
-    name = "pvc-gogs-mysql"
+    name = "pvc-jenkins"
   }
   spec {
     access_modes = ["ReadWriteOnce"]
@@ -182,59 +98,24 @@ resource "kubernetes_persistent_volume_claim" "pvc_gogs-mysql" {
         storage = "2Gi"
       }
     }
-    volume_name = "${kubernetes_persistent_volume.pv_gogs-mysql.metadata.0.name}"
+    volume_name = "${kubernetes_persistent_volume.pv_jenkins.metadata.0.name}"
   }
 }
 
-resource "kubernetes_persistent_volume" "pv_gogs-mysql" {
+resource "kubernetes_persistent_volume" "pv_jenkins" {
   metadata {
-    name = "pv-gogs-mysql"
+    name = "pv-jenkins"
    }
   spec {
     capacity = {
-      storage = "5Gi"
+      storage = "2Gi"
     }
     persistent_volume_reclaim_policy = "Retain"
     access_modes = ["ReadWriteOnce"]
     storage_class_name = "hostpath"
     persistent_volume_source {
       host_path  {
-        path  = "/host_mnt/c/Users/Ahmet_Erkoc/.docker/Volumes/gogs-mysql"
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "pvc_gogs" {
-  metadata {
-    name = "pvc-gogs"
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "hostpath"
-    resources {
-      requests = {
-        storage = "2Gi"
-      }
-    }
-    volume_name = "${kubernetes_persistent_volume.pv_gogs.metadata.0.name}"
-  }
-}
-
-resource "kubernetes_persistent_volume" "pv_gogs" {
-  metadata {
-    name = "pv-gogs"
-   }
-  spec {
-    capacity = {
-      storage = "5Gi"
-    }
-    persistent_volume_reclaim_policy = "Retain"
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "hostpath"
-    persistent_volume_source {
-      host_path  {
-        path  = "/host_mnt/c/Users/Ahmet_Erkoc/.docker/Volumes/gogs"
+        path  = "/host_mnt/c/Users/Ahmet_Erkoc/.docker/Volumes/jenkins"
       }
     }
   }
